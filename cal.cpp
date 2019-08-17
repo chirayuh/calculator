1bc7aa1032943da36e1134b5056d92502a631d01
full meals <chirayuh69@gmail.com>
2019-08-17 11:55:31 +0530

    Added +,-,/,* functions

--- cal.cpp
+++ cal.cpp
@@ -1,3 +1,21 @@
+#include<iostream>
+using namespace std;
+void add(int a,int b)
+{
+	cout<<a+b<<endl;
+}
+void sub(int a,int b)
+{
+	cout<<a-b<<endl;
+}
+void mul(int a,int b)
+{
+	cout<<a*b<<endl;
+}
+void div(int a,int b)
+{
+	cout<<a/b<<endl;
+}
 void main()
 {
 	int a,b;
