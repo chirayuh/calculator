4ad45678f39665c5b4cee7042a47350897f2ca0b
harshit kumar <harshitk980@GMAIL.COM>
2019-08-17 11:53:37 +0530

    added main func

--- cal.cpp
+++ cal.cpp
@@ -1,6 +1,28 @@
 void main()
 {
-	int a,b;
+	int a,b,ch;
+	cout<<"enter two no.s"<<endl;
+	cin>>a;
+	cin>>b;
+	cout<<"enter your choice"<<endl;
+	cin<<ch;
+	switch(ch)
+	{
+		case '+':
+				 add(a,b);
+				 break;
+		case '-':
+				 sub(a,b);
+				 break;
+		case '*':
+				 mul(a,b);
+				 break;
+		case '/':
+				 div(a,b);
+				 break;		 		 		 
+
+	}
+
 	
 
 }
